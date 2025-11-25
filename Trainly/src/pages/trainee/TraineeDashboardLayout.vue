6041a8c48962f873f671ec48a1d7325282139399
Maryam Hassan <mh2207128@gmail.com>
2025-11-25 16:47:06 +0200

    min

--- Trainly/src/pages/trainee/TraineeDashboardLayout.vue
+++ Trainly/src/pages/trainee/TraineeDashboardLayout.vue
@@ -2,7 +2,7 @@
   <div class="flex">
     <SideBarTrainee />
 
-    <div class="flex-1 lg:ml-64 p-10 min-h-screen">
+    <div class="flex-1  lg:ml-64 p-10 min-h-screen">
       <router-view />
     </div>
   </div>
